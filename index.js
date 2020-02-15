85272058f90b721ec88fdefeb64d7f8890bebd91
mocmeo19 <nguyenhoainamnt70@gmail.com>
2020-02-15 13:22:22 +0700

    start

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var setup = require('proxy');
 
 var server = setup(http.createServer());
-server.listen(3128, function () {
+server.listen(process.env.PORT || 3128, function () {
   var port = server.address().port;
   console.log('HTTP(s) proxy server listening on port %d', port);
 });
